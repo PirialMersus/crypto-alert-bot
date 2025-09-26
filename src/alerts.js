71661484f4572bae5de60de30ea9a84e3ca8a84b
pirialmersus <pirialmersus@gmail.com>
2025-09-26 18:21:23 +0300

    en alerts fix

--- src/alerts.js
+++ src/alerts.js
@@ -1,4 +1,5 @@
-import { ENTRIES_PER_PAGE, BG_CHECK_INTERVAL } from './constants.js';
+// src/alerts.js
+import { ENTRIES_PER_PAGE, BG_CHECK_INTERVAL, DELETE_LABEL_TARGET_LEN } from './constants.js';
 import { alertsCollection, alertsArchiveCollection, usersCollection } from './db.js';
 import { tickersCache, pricesCache, allAlertsCache, getUserAlertsCached, getAllAlertsCached, getUserLastViews, setUserLastViews, invalidateUserAlertsCache, getUserAlertsOrder } from './cache.js';
 import { getPriceLevel1 } from './prices.js';
